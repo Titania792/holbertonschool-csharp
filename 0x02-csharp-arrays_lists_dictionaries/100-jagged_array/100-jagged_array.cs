7a44c3826782ec2654bd7856c913ad065839a0f0
Titania792 <tatifernandez34@gmail.com>
2022-11-30 18:01:25 -0300

    advanced task

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedArray = new int[3][];
+            jaggedArray[0] = new int[] { 0, 1, 2, 3 };
+            jaggedArray[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+            jaggedArray[2] = new int[] { 0, 1 };
+
+            for (int row = 0; row < jaggedArray.Length; row++)
+            {
+                for (int item = 0; item < jaggedArray[row].Length; item++)
+                {
+                    Console.Write(jaggedArray[row][item]);
+                    if (item != jaggedArray[row].Length-1)
+                        Console.Write(' ');
+                }
+                Console.WriteLine();
+            } 
+        }
+    }
+}
