56e0d3c1f6a881161dce63bb67bb8cecdb79a75c
Titania792 <tatifernandez34@gmail.com>
2022-11-30 22:05:25 -0300

    task 14

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -6,7 +6,19 @@ namespace _14_rectangular_array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] matrix = new int[5, 5];
+            matrix[2,2] = 1;
+            for (int i = 0; i<5; i++)
+            {
+                for (int j = 0; j<5; j++)
+                {
+                    Console.Write("{0}", matrix[i,j]);
+                    if (j == 4)
+                        Console.WriteLine();
+                    else
+                        Console.Write(" ");
+                }
+            }
         }
     }
 }
