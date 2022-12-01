469d312ebeb30bcffd94cbab6aebb0fc67677da8
Titania792 <tatifernandez34@gmail.com>
2022-12-01 16:21:47 -0300

    task 1

--- 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _1_common_elements
+class List
 {
-    class Program
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
     {
-        static void Main(string[] args)
+        //convert list into hashset to have all uniq values
+        HashSet<int> uniql1 = new HashSet<int>(list1);
+        HashSet<int> uniql2 = new HashSet<int>(list2);
+        uniql1.IntersectWith(uniql2);
+        List<int> commonelem = new List<int>();
+        foreach (var item in uniql1)
         {
-            Console.WriteLine("Hello World!");
+            commonelem.Add(item);
         }
+        return commonelem;
     }
 }
