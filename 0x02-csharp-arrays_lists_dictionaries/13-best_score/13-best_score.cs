81fb6ad70db85a03b4ab7c4a99b60abb57c0fdd3
Titania792 <tatifernandez34@gmail.com>
2022-11-30 21:52:50 -0300

    task 13

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace _13_best_score
+class Dictionary
 {
-    class Program
+    public static string BestScore(Dictionary<string, int> myList)
     {
-        static void Main(string[] args)
+        /*LINQ’s Aggregate() method applies an accumulator function to
+        each item of a sequence. It can be used as follows to find the
+        maximum value of a dictionary*/
+        try
         {
-            Console.WriteLine("Hello World!");
+            var maxValueKey = myList.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+            return maxValueKey;
+        }
+        catch (System.Exception)
+        {
+            return "None";
+            throw;
         }
     }
 }
