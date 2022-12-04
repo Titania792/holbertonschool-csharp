d7ba362091a79f441865c01df2ad61e813fddf01
Titania792 <tatifernandez34@gmail.com>
2022-12-04 03:41:29 -0300

    task 3

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _3_stack_push_pop
+class MyStack
 {
-    class Program
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        static void Main(string[] args)
+        int Nitems = aStack.Count;
+        Console.WriteLine("Number of items: {0}", Nitems);
+        if (Nitems <= 0)
+            Console.WriteLine("Stack is empty");
+        else  
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        
+        if (!aStack.Contains(search))
+            Console.WriteLine("Stack contains \"{0}\": False", search);
+        else
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Stack contains \"{0}\": True", search);
+            while (aStack.Contains(search))
+                aStack.Pop();
         }
+        aStack.Push(newItem);
+        return (aStack);
+
+
+
     }
 }
