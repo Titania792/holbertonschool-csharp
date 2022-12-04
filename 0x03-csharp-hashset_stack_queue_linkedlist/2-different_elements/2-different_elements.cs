555126e00e17600f7a3cba082cb1749eaf039149
Titania792 <tatifernandez34@gmail.com>
2022-12-04 03:32:54 -0300

    task 2

--- 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _2_different_elements
+class List
 {
-    class Program
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        List<int> uniqL1 = list1.FindAll(item => !list2.Contains(item));
+        List<int> uniqL2 = list2.FindAll(item => !list1.Contains(item));
+        // taking both lists and find the items that are present in one of them but not both
+        // (=>) represents lambda operator and/or a separator of a member name and the member implementation in an expression body definition
+        uniqL1.AddRange(uniqL2);
+        uniqL1.Sort();
+        return uniqL1;
     }
 }
