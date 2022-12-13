d464fedf58cbbeafc95cef67423c3400efddf46a
Titania792 <tatifernandez34@gmail.com>
2022-12-13 15:26:07 -0300

    task 1

--- 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -4,6 +4,18 @@ class Int
 {
     public static void divide(int a, int b)
     {
-        Console.WriteLine("Hello World!");
+        int result = 0;
+        try
+        {
+            result = a / b;
+        }
+        catch (System.DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine(a + " / " + b + " = " + result);
+        }
     }
 }
