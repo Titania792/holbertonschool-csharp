d349ca43cf731d540a39f72d4d70f2f85a1284bf
Titania792 <tatifernandez34@gmail.com>
2022-12-14 14:01:50 -0300

    task 3

--- 0x06-csharp-classes/3-enemy/3-enemy.cs
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace _3_enemy
+namespace Enemies
 {
-    class Program
-    {
-        static void Main(string[] args)
+    /// <summary>public class that defines a zombie.</summary>
+    public class Zombie {
+        /// <summary>public int that defines the health of a zombie.</summary>
+        public int health;
+
+        /// <summary>public zombie with health value 0.</summary>
+        public Zombie()
         {
-            Console.WriteLine("Hello World!");
+            health = 0;
+        }
+
+        /// <summary>public zombie with health value as a variable.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        /// <summary>method that returns the value of health of the Zombie object.</summary>
+        public int GetHealth()
+        {
+            return health;
         }
     }
 }
