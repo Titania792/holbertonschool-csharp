b25509bb42190ddba7ca119a7af796912face20e
Titania792 <tatifernandez34@gmail.com>
2022-12-14 14:12:21 -0300

    task 4

--- 0x06-csharp-classes/4-enemy/4-enemy.cs
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 
-namespace _4_enemy
+namespace Enemies
 {
-    class Program
-    {
-        static void Main(string[] args)
+    /// <summary>public class that defines a zombie.</summary>
+    public class Zombie {
+        /// <summary>public int that defines the health of a zombie.</summary>
+        public int health;
+
+        /// <summary>public zombie with health value 0.</summary>
+        public Zombie()
         {
-            Console.WriteLine("Hello World!");
+            health = 0;
+        }
+
+        /// <summary>public zombie with health value as a variable.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        /// <summary>method that returns the value of health of the Zombie object.</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        /// <summary>private field, default value "(No name)".</summary>
+        private string name = "(No name)";
+
+        /// <summary>public property, retrives and sets name</summary>
+        public string Name
+        {
+            get=> name;
+            set=> name = value;
         }
     }
 }
