b5cf4b3f3e503844efcfaccb0ff0faa70c2db97f
Titania792 <tatifernandez34@gmail.com>
2022-12-15 15:12:50 -0300

    task 0

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -3,14 +3,31 @@ using NUnit.Framework;
 namespace MyMath.Tests
 {
     [TestFixture]
+    /// <summary>Tests if Add method is working correctly.</summary>
     public class Tests
     {
         [Test]
-        /// <summary>Tests if Add method is working correctly</summary>
+        /// <summary>Testing with positive numbers.</summary>
         public void Test1()
         {
             int sum = Operations.Add(7, 8);
             Assert.AreEqual(15, sum);
+        }
+
+        [Test]
+        /// <summary> Testing with one negative number.</summary>
+        public void Test2()
+        {
+            int sum = Operations.Add(-7, 8);
+            Assert.AreEqual(1, sum);
+        }
+
+        [Test]
+        /// <summary>Testing with negative numbers.</summary>
+        public void Test3()
+        {
+            int sum = Operations.Add(-7, -8);
+            Assert.AreEqual(-15, sum);
         }
     }
 }
