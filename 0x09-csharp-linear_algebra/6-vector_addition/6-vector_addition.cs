fd63489beda5f180e358f65228ff141996153319
Titania792 <tatifernandez34@gmail.com>
2023-01-11 16:11:17 -0300

    fixing

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -7,6 +7,8 @@ class VectorMath
     /// <summary> method that adds two vectors and returns the resulting vector. </summary>
     public static double[] Add(double[] vector1, double[] vector2)
     {
+        if (vector1.Length != 2 && vector1.Length != 3)
+            return new double[] {-1};
         if (vector2.Length == vector1.Length)
         {
             double[] newvector = new double[vector1.Length];
