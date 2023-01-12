a00735c93528af22aa4046a079557dd4369d32ca
Titania792 <tatifernandez34@gmail.com>
2023-01-12 17:59:23 -0300

    task 24-28

--- 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace _24_matrix_shear_2D
+/// <summary> 22. Matrix shear #1 </summary>
+class MatrixMath
 {
-    class Program
+    /// <summary> method that shears a square 2D matrix by a given shear factor and returns the resulting matrix. </summary>
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
-        static void Main(string[] args)
+        double[,] nmatrix = new double[2,2];
+        double[,] smatrix;
+        
+        if (matrix.GetLength(0) != 2 && matrix.GetLength(1) != 2)
+            return new double[,] {{-1}};
+        if (direction == 'x' || direction == 'y')
         {
-            Console.WriteLine("Hello World!");
+            if (direction == 'x')
+                smatrix = new double[,] {{1, 0}, {factor, 1}};
+            else
+                smatrix = new double[,] {{1, factor}, {0, 1}};
         }
+        else
+            return new double[,] {{-1}};
+        for (int i = 0; i < 2; i++)
+		{
+            for (int j = 0; j < 2; j++)
+			{
+                for (int k = 0; k < 2; k++)
+				{
+                    nmatrix[i, j] += (matrix[i, k] * smatrix[k, j]);
+                }
+            }
+        }
+		return nmatrix;
     }
 }
--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace _28_determinant
+///<summary> 26. Determinant #3 </summary>
+class MatrixMath
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	///<summary> method that calculates the determinant of a matrix. </summary>
+	public static double Determinant(double[,] matrix)
+	{
+		if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+			return Math.Round(((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0])), 2);
+		else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+		{
+			double a = matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 1]));
+			double b = matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 0]));
+			double c = matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0]));
+			return Math.Round((a - b + c),2);
+		}
+		return - 1;
+	}
 }
