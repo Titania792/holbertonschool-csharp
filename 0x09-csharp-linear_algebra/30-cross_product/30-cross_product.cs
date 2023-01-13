6e918f8c9eeacd6153407ea59e7087fb5f33f369
Titania792 <tatifernandez34@gmail.com>
2023-01-13 11:45:57 -0300

    fix

--- 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -7,7 +7,7 @@ class VectorMath
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
         if (vector1.Length != 3 || vector2.Length != 3)
-            return new double[] {};
+            return new double[] {-1};
 
         double[] nvector = new double[vector1.Length];
 
