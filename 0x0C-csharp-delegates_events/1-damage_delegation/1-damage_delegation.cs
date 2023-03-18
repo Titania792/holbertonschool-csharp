0603527cfb15da245297b6f0a37d891d9759294f
Titania792 <tatifernandez34@gmail.com>
2023-03-18 00:56:04 -0300

    🤡

--- 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
-namespace _1_damage_delegation
+/// <summary>
+/// Player Class
+/// </summary>
+public class Player
 {
-    class Program
+    private string name {get; set;}
+    private float maxHp {get; set;}
+    private float hp {get; set;}
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxHp"></param>
+    public Player(string name = "Player", float maxHp = 100f)
     {
-        static void Main(string[] args)
+        this.name = name;
+        if (maxHp > 0)
         {
-            Console.WriteLine("Hello World!");
+            this.maxHp = maxHp;
         }
+        else
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.hp = this.maxHp;
+    }
+
+    /// <summary>
+    /// Method Print
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", this.name,this.hp,this.maxHp);
     }
 }
